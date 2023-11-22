bbac65809a40a95566ca4b733091288f2038ed4f
devilmcry <108988164+MASPR-TECH@users.noreply.github.com>
2023-11-22 17:54:52 +0530

    Updated main.py

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, this is your Flask app!'
+    return 'Hello, this is CI/CD using Github Actions'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
